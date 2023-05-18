471c86ddff216ec5213ec42f24c7da9a303aa8b6
ben lim <lim6112j@gmail.com>
2023-05-18 14:20:41 +0900

    용어 정리 및 커맨트 삭제

--- plot_rl_rewards.py
+++ plot_rl_rewards.py
@@ -10,14 +10,15 @@ args = parser.parse_args()
 
 a = np.load(f'rl_rebalancer_rewards/{args.mode}.npy')
 
-print(f"average reward: {a.mean():.2f}, min: {a.min():.2f}, max: {a.max():.2f}")
+print(
+    f"average reward: {a.mean():.2f}, min: {a.min():.2f}, max: {a.max():.2f}")
 
 if args.mode == 'train':
-  # show the training progress
-  plt.plot(a)
+    # show the training progress
+    plt.plot(a)
 else:
-  # test - show a histogram of rewards
-  plt.hist(a, bins=20)
+    # test - show a histogram of rewards
+    plt.hist(a, bins=20)
 
 plt.title(args.mode)
 plt.show()
